3bce49b0fde0ab81fd7ed4b6aca75087e70736a3
Dmitrii <dmkuz91@gmail.com>
2023-03-22 23:03:52 +0700

    Example004 сложение случайных чисел от 1 до 10

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+﻿double numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
 
